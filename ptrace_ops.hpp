b25530910d026bbd14a221329f86f87f1476ff98
BR4ND0NFL0RES <124124466+BR4ND0NFL0RES@users.noreply.github.com>
2026-05-18 16:54:59 -0600

    Delete ptrace_ops.hpp

--- ptrace_ops.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef PTRACE_OPS_HPP
-#define PTRACE_OPS_HPP
-
-#include <sys/types.h>
-
-// Declaración de la función de inyección de carga útil
-void inject_data(pid_t pid, unsigned long dest_addr, const char *source, int size);
-
-#endif
